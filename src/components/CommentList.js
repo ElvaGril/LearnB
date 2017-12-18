9334af14a771f517e1abea246370c746df1cbacb
pan <Lana6767@163.com>
2017-12-18 23:12:21 +0800

    第一阶段评论功能完成

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -2,24 +2,37 @@ import React, { Component  } from 'react';
 import Comment from './Comment'
 
 class CommentList extends Component  {
-    render() {
-        const comments = [
-            {userName: 'Jerry', content: 'Hello'},
-            {userName: 'Tomy', content: 'World'},
-            {userName: 'Lucy', content: 'Good'}
-        ]
-
-        return (
-            <div>{comments.map((comment,i) => {
-                return (
-                    <div key= {i}>
-                        {comments.map((comment, i) => <Comment comment={comment} key={i} />)}
-                    </div>
-                )
-            })} 
-            </div>
-        )
-    }
+	 static defaultProps = {
+	    comments: []
+	  }
+	 
+//  render() {
+////      const comments = [
+////          {userName: 'Jerry', content: 'Hello'},
+////          {userName: 'Tomy', content: 'World'},
+////          {userName: 'Lucy', content: 'Good'}
+////      ]
+//
+//      return (
+//          <div>{comments.map((comment,i) => {
+//              return (
+//                  <div key= {i}>
+//                      {this.props.comments.map((comment, i) => <Comment comment={comment} key={i} />)}
+//                  </div>
+//              )
+//          })} 
+//          </div>
+//      )
+//  }
+     render() {
+	    return (
+	      <div>
+	        {this.props.comments.map((comment, i) =>
+	          <Comment comment={comment} key={i} />
+	        )}
+	      </div>
+	    )
+	  }
 }
  
 export default CommentList
